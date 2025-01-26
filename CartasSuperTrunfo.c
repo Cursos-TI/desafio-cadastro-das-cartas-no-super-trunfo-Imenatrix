e12d1fd32582dea509347225e6c1cd6f30a8d684
Ruan Gomes <ruangomesdorosario@gmail.com>
2025-01-26 21:47:04 +0000

    Extrai funções operacionais

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,49 +16,65 @@ typedef struct {
     float pib_per_capita;
 } Carta;
 
+void input_carta(Carta *carta);
+void exibir_carta(Carta *carta);
+
 int main() {
     // Declara a carta a ser cadastrada
     Carta carta;
     
     // Cadastro das Cartas:
     // Insere os dados da carta sequencialmente via terminal
-    printf("Digite o estado: ");
-    scanf("%c", &carta.estado);
-
-    printf("Digite o código da carta: ");
-    scanf("%3s", carta.codigo);
-
-    printf("Digite o nome da cidade: ");
-    scanf("%255s", carta.nome);
-
-    printf("Digite a população: ");
-    scanf("%i", &carta.populacao);
-
-    printf("Digite a área: ");
-    scanf("%f", &carta.area);
-
-    printf("Digite o PIB: ");
-    scanf("%f", &carta.pib);
-
-    printf("Digite o número de pontos turísticos: ");
-    scanf("%i", &carta.numero_pontos_turisticos);
-
-    // Propriedades calculadas
-    carta.densidade_populacional = carta.populacao / carta.area;
-    carta.pib_per_capita = carta.pib / carta.populacao;
+    input_carta(&carta);
 
     // Exibição dos Dados das Cartas:
     // Imprime os dados da carta sequencialmente no terminal
     printf("\n");
-    printf("Estado: %c\n", carta.estado);
-    printf("Código da Carta: %s\n", carta.codigo);
-    printf("Nome da Cidade: %s\n", carta.nome);
-    printf("População: %i\n", carta.populacao);
-    printf("Área: %.2f km²\n", carta.area);
-    printf("PIB: %.2f bilhões de reais\n", carta.pib);
-    printf("Número de Pontos Turísticos: %i\n", carta.numero_pontos_turisticos);
-    printf("Densidade populacional: %.2f individuos por km²\n", carta.densidade_populacional);
-    printf("PIB per capita: %.2f bilhões de reais por pessoa\n", carta.pib_per_capita);
+    exibir_carta(&carta);
 
     return 0;
+}
+
+// Pede as informações de uma carta ao usuário
+// E armazena as resposta na carta cujo endereço
+// É passado
+void input_carta(Carta *carta) {
+    printf("Digite o estado: ");
+    scanf("%c", &carta->estado);
+
+    printf("Digite o código da carta: ");
+    scanf("%3s", carta->codigo);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%255s", carta->nome);
+
+    printf("Digite a população: ");
+    scanf("%i", &carta->populacao);
+
+    printf("Digite a área: ");
+    scanf("%f", &carta->area);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &carta->pib);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%i", &carta->numero_pontos_turisticos);
+
+    // Propriedades calculadas
+    carta->densidade_populacional = carta->populacao / carta->area;
+    carta->pib_per_capita = carta->pib / carta->populacao;
+}
+
+// Exibe as informações da carta no terminal
+// de forma estruturada
+void exibir_carta(Carta *carta) {
+    printf("Estado: %c\n", carta->estado);
+    printf("Código da Carta: %s\n", carta->codigo);
+    printf("Nome da Cidade: %s\n", carta->nome);
+    printf("População: %i\n", carta->populacao);
+    printf("Área: %.2f km²\n", carta->area);
+    printf("PIB: %.2f bilhões de reais\n", carta->pib);
+    printf("Número de Pontos Turísticos: %i\n", carta->numero_pontos_turisticos);
+    printf("Densidade populacional: %.2f individuos por km²\n", carta->densidade_populacional);
+    printf("PIB per capita: %.2f bilhões de reais por pessoa\n", carta->pib_per_capita);
 }
